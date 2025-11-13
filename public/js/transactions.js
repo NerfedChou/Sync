be2c9c1a5696eb6890c99e1349874984231912db
chef <janrhian012904@gmail.com>
2025-11-13 08:04:04 +0800

    Refractored

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -135,15 +135,8 @@ class TransactionsPage {
                 </span>
             </td>
             <td class="transaction-actions">
-                <button class="btn-icon btn-icon--edit" onclick="transactionsPage.editTransaction(${transaction.id})" title="Edit">
-                    <svg viewBox="0 0 24 24">
-                        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
-                    </svg>
-                </button>
-                <button class="btn-icon btn-icon--delete" onclick="transactionsPage.deleteTransaction(${transaction.id})" title="Delete">
-                    <svg viewBox="0 0 24 24">
-                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-                    </svg>
+                <button class="btn btn-actions">
+                    <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
                 </button>
             </td>
         `;
